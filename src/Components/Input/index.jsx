6f227dfc6bc87c6ce98da919f393553ae383d3ec
unknown <deividibaby6@gmail.com>
2023-04-06 02:51:15 -0400

    create ClassName

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -2,7 +2,7 @@ import './style.scss'
 
 export default function Input({ placeholder, label, onChange, type }) {
   return (
-    <div>
+    <div className="input-class">
       <label>{label}</label>
       <input placeholder={placeholder} onChange={onChange} type={type}></input>
     </div>
